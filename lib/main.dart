a2606040f99d42b08df3e2071e902e73348808ee
Aakarsh Goyal <aakarshgoyal23@gmail.com>
2024-07-19 12:36:53 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'onboarding/welcome.dart'; 
+import 'onboarding/welcome.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 void main() {
@@ -11,17 +11,16 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
     final textTheme = GoogleFonts.poppinsTextTheme();
 
     return MaterialApp(
       title: 'Soraya',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSwatch().copyWith(
-          primary: const Color(0xFFFF5D8F), 
-          secondary: const Color(0xFFFFC4D6), 
+          primary: const Color(0xFFFF5D8F),
+          secondary: const Color(0xFFFFC4D6),
           brightness: Brightness.light,
-          
         ),
         textTheme: textTheme.apply(),
         // primarySwatch: Colors.deepPurple,
