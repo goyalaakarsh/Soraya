8b9fce96dadf9b373810b326022fe1e147b33ad3
Aakarsh Goyal <aakarshgoyal23@gmail.com>
2024-09-15 14:02:19 +0530

    Update login.dart

--- lib/onboarding/login.dart
+++ lib/onboarding/login.dart
@@ -11,90 +11,92 @@ class Login extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body:
-      SizedBox.expand(
-        child: Stack(children:[ Image.asset("assets/images/welcome_bg.jpg",
-        fit: BoxFit.cover, width: double.infinity, height: double.infinity,),
-        LoginPage(),
-      ],),),
-       
-      );
-    
+      body: SizedBox.expand(
+        child: Stack(
+          children: [
+            Image.asset(
+              "assets/images/welcome_bg.jpg",
+              fit: BoxFit.cover,
+              width: double.infinity,
+              height: double.infinity,
+            ),
+            LoginPage(),
+          ],
+        ),
+      ),
+    );
   }
 }
 
-class LoginPage extends StatefulWidget{
+class LoginPage extends StatefulWidget {
   @override
-  State<LoginPage> createState()=> LoginPageState();
+  State<LoginPage> createState() => LoginPageState();
 }
 
-class LoginPageState extends State<LoginPage>{
+class LoginPageState extends State<LoginPage> {
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     final Color primaryColor = Theme.of(context).colorScheme.primary;
     final Color secondaryColor = Theme.of(context).colorScheme.secondary;
     return Container(
-  child: Center(
-    child: SingleChildScrollView(
-      child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            SizedBox(
-              height: 20,
-            ),
-            SizedBox(
-              height: 65,
-              child: DefaultTextStyle(
-              style: GoogleFonts.kalnia(
-              color: primaryColor ,
-              fontSize: 50,
-              fontWeight: FontWeight.w600,
-              ),
-              child: AnimatedTextKit(
-              repeatForever: true,
-              isRepeatingAnimation: true,
-              animatedTexts: [
-              TyperAnimatedText('Welcome to'),
-              TyperAnimatedText('Soraya'),
-              
-                
-      ],
-    ),
-  ),
-  
-),
-
-SizedBox(height: 20,),
-
- Container(
-  decoration: BoxDecoration(
-    // color: Color.fromARGB(232, 170, 64, 241), 
-    borderRadius: BorderRadius.circular(12),
-  ),
-  child: Center(
-    child: SvgPicture.asset(
-                'assets/images/welcome_illus.svg',
-                width: 150,
-                height: 150,
-              ),
-  ),
-),
-
-
-          SizedBox(height: 18),
-               Center(
-                 child: Container(
-                    child: Text("Login", style: TextStyle(fontSize: 33, fontWeight: FontWeight.bold),),
+      child: Center(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                SizedBox(
+                  height: 20,
+                ),
+                SizedBox(
+                  height: 65,
+                  child: DefaultTextStyle(
+                    style: GoogleFonts.kalnia(
+                      color: primaryColor,
+                      fontSize: 50,
+                      fontWeight: FontWeight.w600,
+                    ),
+                    child: AnimatedTextKit(
+                      repeatForever: true,
+                      isRepeatingAnimation: true,
+                      animatedTexts: [
+                        TyperAnimatedText('Welcome to'),
+                        TyperAnimatedText('Soraya'),
+                      ],
+                    ),
                   ),
-               ),
-
-               SizedBox(height:20), 
-              Container(
-                width: 280,
-                child: TextField(
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                    // color: Color.fromARGB(232, 170, 64, 241),
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  child: Center(
+                    child: SvgPicture.asset(
+                      'assets/images/welcome_illus.svg',
+                      width: 150,
+                      height: 150,
+                    ),
+                  ),
+                ),
+                SizedBox(height: 18),
+                Center(
+                  child: Container(
+                    child: Text(
+                      "Login",
+                      style: TextStyle(fontSize: 33, fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 20),
+                Container(
+                  width: 280,
+                  child: TextField(
                     decoration: InputDecoration(
                       prefixIcon: Icon(Icons.email_outlined),
                       labelText: 'Username',
@@ -116,13 +118,12 @@ SizedBox(height: 20,),
                       ),
                     ),
                   ),
-              ),
-
-              SizedBox(height:20),
-              Container(
-                width: 280,
-                child: TextField(
-                  obscureText: true,
+                ),
+                SizedBox(height: 20),
+                Container(
+                  width: 280,
+                  child: TextField(
+                    obscureText: true,
                     decoration: InputDecoration(
                       prefixIcon: Icon(Icons.lock),
                       labelText: 'Password',
@@ -144,56 +145,69 @@ SizedBox(height: 20,),
                       ),
                     ),
                   ),
-              ),
-              SizedBox(height: 22),  
-              Container(
-              decoration: BoxDecoration(
-                gradient: LinearGradient(
-                  colors: [primaryColor, secondaryColor],
-                  begin: Alignment.topLeft,
-                  end: Alignment.bottomRight,
                 ),
-                borderRadius: BorderRadius.circular(22.0),
-              ),
-              child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  minimumSize: Size(250, 47),
-                  backgroundColor: Colors.transparent,
-                  shadowColor: Colors.transparent,
-                  shape: RoundedRectangleBorder(
+                SizedBox(height: 22),
+                Container(
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                      colors: [
+                        primaryColor,
+                        secondaryColor
+                      ],
+                      begin: Alignment.topLeft,
+                      end: Alignment.bottomRight,
+                    ),
                     borderRadius: BorderRadius.circular(22.0),
                   ),
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      minimumSize: Size(250, 47),
+                      backgroundColor: Colors.transparent,
+                      shadowColor: Colors.transparent,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(22.0),
+                      ),
+                    ),
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => Home()),
+                      );
+                    },
+                    child: Text(
+                      'Login',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
                 ),
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => Home()),
-                  );
-                },
-                child: Text(
-                  'Login',
-                  style: TextStyle(color: Colors.white),
-                ),
-              ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Dont have an account yet?',
+                      style: TextStyle(fontWeight: FontWeight.bold, color: Colors.grey[850]),
+                    ),
+                    TextButton(
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (context) => Signup()),
+                          );
+                        },
+                        child: Text(
+                          'Sign up',
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                        style: TextButton.styleFrom(
+                          padding: EdgeInsets.zero,
+                        ))
+                  ],
+                )
+              ],
             ),
-
-
-          Row(            
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text('Dont have an account yet?',style: TextStyle(fontWeight: FontWeight.bold,color: Colors.grey[850]),),
-              TextButton(onPressed: (){
-                Navigator.push(context, MaterialPageRoute(builder: (context) => Signup()),);
-                
-              }, child: Text('Sign up',style: TextStyle(fontWeight: FontWeight.bold),),style: TextButton.styleFrom(
-          padding: EdgeInsets.zero,))
-            ],
-           )       
-          
-          ],
-        ),),),
-
-    ),);
+          ),
+        ),
+      ),
+    );
   }
 }
-
